b108b196dafcd5e669d62d4aa081a081b4c575ae
Anderson Resende <anderson@vinta.com.br>
2024-11-07 15:36:14 -0300

    Fixed .env reading location

--- fastapi_backend/app/src/config.py
+++ fastapi_backend/app/src/config.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     EXPIRE_ON_COMMIT: bool = False
 
     model_config = SettingsConfigDict(
-        env_file="app/.env", env_file_encoding="utf-8", extra="ignore"
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
 
 
