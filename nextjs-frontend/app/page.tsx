6b514e8ea4178bec1ed6d893a57ee374a3f938ca
Anderson Resende <anderson@vinta.com.br>
2024-11-08 11:27:42 -0300

    Added frontend rules to pre-commit

--- nextjs-frontend/app/page.tsx
+++ nextjs-frontend/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-    return (
-        <h1 className="text-3xl font-bold underline">
-            Home!
-        </h1>
-    );
+  return <h1 className="text-3xl font-bold underline">Home!</h1>;
 }
--- nextjs-frontend/app/register/page.tsx
+++ nextjs-frontend/app/register/page.tsx
@@ -1,48 +1,54 @@
-"use client"
+"use client";
 
 import {
-    Card,
-    CardContent, CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
-import {Input} from "@/components/ui/input"
-import {Label} from "@/components/ui/label"
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
-import {register} from "@/components/actions/register-action";
-import {useFormState} from "react-dom";
-import {SubmitButton} from "@/components/ui/submitButton";
-
+import { register } from "@/components/actions/register-action";
+import { useFormState } from "react-dom";
+import { SubmitButton } from "@/components/ui/submitButton";
 
 export default function Page() {
-    const [state, dispatch] = useFormState(register, {message: ""})
-    return (
-        <div className="flex h-screen w-full items-center justify-center px-4">
-            <form action={dispatch}>
-                <Card className="w-full max-w-sm">
-                    <CardHeader>
-                        <CardTitle className="text-2xl">Signup</CardTitle>
-                        <CardDescription>
-                            Enter your email and password below to create your account.
-                        </CardDescription>
-                    </CardHeader>
-                    <CardContent className="grid gap-4">
-                        <div className="grid gap-2">
-                            <Label htmlFor="email">Email</Label>
-                            <Input id="email" name="email" type="email" placeholder="m@example.com" required/>
-                        </div>
-                        <div className="grid gap-2">
-                            <Label htmlFor="password">Password</Label>
-                            <Input id="password" name="password" type="password" required/>
-                        </div>
-                    </CardContent>
-                    <CardFooter>
-                        <SubmitButton/>
-                        <div>{state?.message && <p>{state.message}</p>}</div>
-                    </CardFooter>
-                </Card>
-            </form>
-        </div>
-    )
+  const [state, dispatch] = useFormState(register, { message: "" });
+  return (
+    <div className="flex h-screen w-full items-center justify-center px-4">
+      <form action={dispatch}>
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-2xl">Signup</CardTitle>
+            <CardDescription>
+              Enter your email and password below to create your account.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" name="password" type="password" required />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <SubmitButton />
+            <div>{state?.message && <p>{state.message}</p>}</div>
+          </CardFooter>
+        </Card>
+      </form>
+    </div>
+  );
 }
