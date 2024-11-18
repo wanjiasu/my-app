c16b2e921c64158a117a6a72ad3232231d955966
Anderson Resende <anderson@vinta.com.br>
2024-11-18 17:52:47 -0300

    Adjusted settings config

--- fastapi_backend/app/src/config.py
+++ fastapi_backend/app/src/config.py
@@ -2,17 +2,24 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    # Database
     DATABASE_URL: str
+    EXPIRE_ON_COMMIT: bool = False
+
+    # User
     ACCESS_SECRET_KEY: str
     RESET_PASSWORD_SECRET_KEY: str
     VERIFICATION_SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_SECONDS: int = 3600
-    EXPIRE_ON_COMMIT: bool = False
-    MAIL_USERNAME: str
-    MAIL_PASSWORD: str
-    MAIL_FROM: str
-    FRONTEND_URL: str
+
+    # Email
+    MAIL_USERNAME: str | None = None
+    MAIL_PASSWORD: str | None = None
+    MAIL_FROM: str | None = None
+
+    # Frontend
+    FRONTEND_URL: str | None = None
 
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
