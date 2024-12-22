d8a55ec011e0f3bb9feb9b4549dced285371745d
Anderson Resende <anderson@vinta.com.br>
2024-12-22 20:05:15 -0300

    Axious configuration

--- nextjs-frontend/lib/clientConfig.ts
+++ nextjs-frontend/lib/clientConfig.ts
@@ -1,49 +1,11 @@
-import { client } from "@/app/openapi-client/services.gen";
+import { client } from "@/app/openapi-client/sdk.gen";
 
 const configureClient = () => {
-  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+  const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
   client.setConfig({
-    baseUrl: baseUrl,
+    baseURL: baseURL,
   });
 };
 
-client.interceptors.request.use((request, options) => {
-  if (options.body) {
-    const { contentLength } = calculateContentLength(options.body);
-    request.headers.set("Content-Length", contentLength.toString());
-  }
-  return request;
-});
-
 configureClient();
-
-type RequestBody =
-  | URLSearchParams
-  | string
-  | object
-  | number
-  | null
-  | undefined;
-
-function calculateContentLength(body: RequestBody) {
-  let requestBody: string;
-
-  // Determine the body type and handle accordingly
-  if (body instanceof URLSearchParams) {
-    requestBody = body.toString();
-  } else if (typeof body === "string" || typeof body === "number") {
-    requestBody = body.toString();
-  } else if (body) {
-    requestBody = JSON.stringify(body);
-  } else {
-    requestBody = ""; // Handle null or undefined body
-  }
-
-  // Calculate the byte length using UTF-8 encoding
-  const contentLength = Buffer.byteLength(requestBody, "utf8");
-
-  return {
-    contentLength,
-  };
-}
